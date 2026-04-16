d4726a9126efde253d7d406600ff2d399e6d2f09
muhammad syaifulloh <skyfoxmarket@gmail.com>
2026-04-16 17:04:16 +0700

    update layouts DashboardView pakai reusable compoenent

--- frontend/src/components/admin/Sidebar.vue
+++ frontend/src/components/admin/Sidebar.vue
@@ -1,6 +1,8 @@
 <template>
+    <div v-if="isOpen" @click="$emit('update:isOpen', false)" class="fixed inset-0 bg-slate-900/50 z-[60] lg:hidden backdrop-blur-sm cursor-pointer"></div>
+
     <aside :class="isOpen ? 'translate-x-0' : '-translate-x-full lg:translate-x-0'"
-        class="fixed lg:sticky top-0 inset-y-0 left-0 w-64 bg-slate-900 flex flex-col z-70 transition-transform duration-300 ease-in-out h-screen">
+        class="fixed lg:sticky top-0 inset-y-0 left-0 w-64 bg-slate-900 flex flex-col z-[70] transition-transform duration-300 ease-in-out h-screen">
         <div class="p-6 border-b border-slate-800 flex justify-between items-center">
             <div class="flex items-center gap-3">
                 <div class="h-8 w-8 bg-[#ea4435] rounded-lg flex items-center justify-center text-white font-black">
@@ -51,9 +53,7 @@ const props = defineProps({
 const emit = defineEmits(['update:isOpen', 'update:currentView', 'logout'])
 
 const changeView = (view) => {
-    // Memberitahu DashboardView bahwa menu diklik
     emit('update:currentView', view)
-    // Otomatis menutup sidebar di tampilan mobile setelah menu diklik
     emit('update:isOpen', false)
 }
 </script>
